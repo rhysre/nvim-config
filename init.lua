cba0d1eaba7fba3e9f5e0a130b8358becbe9349b
Rhys Rustad-Elliott <rhys.rustad-elliott@elastic.co>
2023-02-22 22:52:45 +0000

    Use vim.lsp.buf.format over deprecated function

--- init.lua
+++ init.lua
@@ -161,7 +161,7 @@ end
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.go',
   callback = function()
-    vim.lsp.buf.formatting_sync()
+    vim.lsp.buf.format()
     go_org_imports()
   end,
 })
