b9b66483e9bf97f8b6d5d759f325b55394ebf2ec
Rhys Rustad-Elliott <rhys.rustad-elliott@elastic.co>
2023-03-24 21:37:46 +0000

    Add back removed keybindings, comment out yamlls

--- init.lua
+++ init.lua
@@ -64,8 +64,24 @@ require'treesitter-context'.setup()
 
 local lspconfig = require('lspconfig')
 
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+end
+
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'zls', 'pylsp', 'gopls', 'bashls' }
+local servers = { 'zls', 'pylsp', 'gopls', 'bashls' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
@@ -73,6 +89,8 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+-- Not currently using yamlls, but leave this here in case I do in the future
+--[[
 lspconfig.yamlls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
@@ -87,6 +105,7 @@ lspconfig.yamlls.setup {
     },
   },
 }
+]]
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<Leader>ff', builtin.find_files, { desc = "find files" })
