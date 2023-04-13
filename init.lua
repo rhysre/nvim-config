8f799d4d9e96cd753a886e82d3de16d12ee5d629
Rhys Rustad-Elliott <rhys.rustad-elliott@elastic.co>
2023-04-13 23:28:35 +0000

    Add go plugin

--- init.lua
+++ init.lua
@@ -30,6 +30,7 @@ packer.startup(function()
   use 'ntpeters/vim-better-whitespace'
   use 'ray-x/lsp_signature.nvim'
   use 'glepnir/lspsaga.nvim'
+  use { 'fatih/vim-go', run = ':GoUpdateBinaries' }
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
@@ -76,8 +77,10 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+
+  vim.keymap.set('n', '<leader>gc', ':GoCoverageToggle<cr>', bufopts)
 end
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
@@ -160,27 +163,4 @@ ftplugin('asciidoc', {
 ftplugin('go', {
   tabstop = 4,
   expandtab = false
-})
-
--- auto format for gopls
-local go_org_imports = function(wait_ms)
-  local params = vim.lsp.util.make_range_params()
-  params.context = {only = {"source.organizeImports"}}
-  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-  for cid, res in pairs(result or {}) do
-    for _, r in pairs(res.result or {}) do
-      if r.edit then
-        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-        vim.lsp.util.apply_workspace_edit(r.edit, enc)
-      end
-    end
-  end
-end
-
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
-  callback = function()
-    vim.lsp.buf.format()
-    go_org_imports()
-  end,
 })
